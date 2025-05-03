773038d2e201d5d3f8517ce27d0d3d1d636f5541
Peulto <61219276+turtle85917@users.noreply.github.com>
2025-05-03 11:37:04 +0900

    chore: Install `elixir_uuid`

--- lib/hello_elixir.ex
+++ lib/hello_elixir.ex
@@ -1,7 +1,9 @@
 defmodule HelloElixir do
   use Application
+  alias UUID
+
   def start(_type, _args) do
-    IO.puts(HelloElixir.hello())
+    IO.puts(UUID.uuid4())
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
